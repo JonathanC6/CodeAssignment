300740cfb1f30340a264f069a7b663d4e5fdf5ed
JonathanC6 <131599780+JonathanC6@users.noreply.github.com>
2024-04-08 23:08:43 +0800

    Add files via upload

---
+++ 第一次作业/Calculator/PrimeNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeFactors
+{
+    class Program
+    {
+        static void PrintPrimeFactors(int n)
+        {
+            for (int i = 2; i <= n; i++)
+            {
+                while (n % i == 0)
+                {
+                    Console.WriteLine(i);
+                    n /= i;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("请输入一个正整数：");
+            if (int.TryParse(Console.ReadLine(), out int number) && number > 0)
+            {
+                Console.WriteLine($"数字{number}的素数因子为：");
+                PrintPrimeFactors(number);
+            }
+            else
+            {
+                Console.WriteLine("输入无效，请输入一个正整数");
+            }
+            Console.ReadKey();
+        }
+    }
+}
