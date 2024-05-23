3566f8a033f5f496635bc137e92e4c0c836058fc
JonathanC6 <131599780+JonathanC6@users.noreply.github.com>
2024-05-23 22:42:40 +0800

    Add files via upload

---
+++ 第六次作业/f1/f1/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace f1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            splitContainer1 = new SplitContainer();
+            treeView1 = new TreeView();
+            listView1 = new ListView();
+            menuStrip1 = new MenuStrip();
+            文件ToolStripMenuItem = new ToolStripMenuItem();
+            toolStrip1 = new ToolStrip();
+            toolStripButton1 = new ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            menuStrip1.SuspendLayout();
+            toolStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 25);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(listView1);
+            splitContainer1.Size = new Size(1207, 702);
+            splitContainer1.SplitterDistance = 525;
+            splitContainer1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            treeView1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            treeView1.BackColor = SystemColors.Info;
+            treeView1.Location = new Point(22, 56);
+            treeView1.Name = "treeView1";
+            treeView1.Size = new Size(487, 634);
+            treeView1.TabIndex = 0;
+            treeView1.AfterSelect += treeView1_AfterSelect;
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(21, 56);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(634, 634);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.MouseDoubleClick += listView1_DoubleClick;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(32, 32);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { 文件ToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1207, 25);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            文件ToolStripMenuItem.Size = new Size(44, 21);
+            文件ToolStripMenuItem.Text = "文件";
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.ImageScalingSize = new Size(32, 32);
+            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripButton1 });
+            toolStrip1.Location = new Point(0, 25);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Padding = new Padding(0, 0, 2, 0);
+            toolStrip1.Size = new Size(1207, 39);
+            toolStrip1.TabIndex = 2;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            toolStripButton1.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton1.Image = (Image)resources.GetObject("toolStripButton1.Image");
+            toolStripButton1.ImageTransparentColor = Color.Magenta;
+            toolStripButton1.Name = "toolStripButton1";
+            toolStripButton1.Size = new Size(36, 36);
+            toolStripButton1.Text = "刷新";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1207, 727);
+            Controls.Add(toolStrip1);
+            Controls.Add(splitContainer1);
+            Controls.Add(menuStrip1);
+            Name = "Form1";
+            Text = "Form1";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer1;
+        private TreeView treeView1;
+        private MenuStrip menuStrip1;
+        private ToolStrip toolStrip1;
+        private ToolStripMenuItem 文件ToolStripMenuItem;
+        private ToolStripButton toolStripButton1;
+        private ListView listView1;
+    }
+}
---
+++ 第六次作业/f1/f1/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace f1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTreeView();
+            InitializeListView();
+        }
+
+        private void InitializeTreeView()
+        {
+            TreeNode rootNode = new TreeNode("C:\\");
+            rootNode.Tag = "C:\\";
+            GetDirectories(rootNode, 1);
+            treeView1.Nodes.Add(rootNode);
+        }
+
+        // 初始化 ListView
+        private void InitializeListView()
+        {
+            listView1.View = View.Details;
+            listView1.FullRowSelect = true;
+            listView1.GridLines = true;
+
+            // 添加列
+            listView1.Columns.Add("Name", 150);  // 150 是列宽
+            listView1.Columns.Add("Full Path", 300);
+
+            // 如果你想添加图标
+            ImageList imgList = new ImageList();
+            imgList.Images.Add(SystemIcons.WinLogo.ToBitmap());
+            imgList.Images.Add(SystemIcons.WinLogo.ToBitmap());
+            listView1.SmallImageList = imgList;
+        }
+
+        private void GetDirectories(TreeNode node, int depth)
+        {
+            if (depth > 4)
+            {
+                return;
+            }
+
+            string path = (string)node.Tag;
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+            try
+            {
+                foreach (var directory in directoryInfo.GetDirectories())
+                {
+                    TreeNode newnode = new TreeNode(directory.Name);
+                    newnode.Tag = directory.FullName;
+                    node.Nodes.Add(newnode);
+                    GetDirectories(newnode, depth + 1);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string path = (string)e.Node.Tag;
+            PopulateListView(path);
+        }
+
+        private void PopulateListView(string path)
+        {
+            listView1.Items.Clear();
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+            try
+            {
+                foreach (var directory in directoryInfo.GetDirectories())
+                {
+                    ListViewItem listViewItem = new ListViewItem(directory.Name, 0);
+                    listViewItem.Tag = directory.FullName;
+                    AddItemToListView(directory.Name, directory.FullName, 0);
+                }
+                foreach (var directory in directoryInfo.GetFiles())
+                {
+                    //ListViewItem listViewItem = new ListViewItem(directory.FullName, 1);
+                    //listViewItem.Tag = directory.FullName;
+                    AddItemToListView(directory.Name, directory.FullName, 1);
+                }
+            }
+            catch (Exception ex)
+            {
+                // 处理异常，比如弹窗通知用户
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // 添加文件或文件夹
+        private void AddItemToListView(string name, string fullPath, int iconIndex)
+        {
+            ListViewItem item = new ListViewItem(name, iconIndex);
+            item.SubItems.Add(fullPath);  // 添加到第二列
+            listView1.Items.Add(item);
+        }
+
+        private void listView1_DoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                string fullPath = listView1.SelectedItems[0].SubItems[1].Text;
+                if (Directory.Exists(fullPath))
+                {
+                    // 如果是文件夹，刷新ListView
+                    PopulateListView(fullPath);
+                }
+                else if (File.Exists(fullPath))
+                {
+                    // 根据文件类型决定打开方式
+                    string extension = Path.GetExtension(fullPath).ToLower();
+                    if (extension == ".txt")
+                    {
+                        Process.Start("notepad.exe", fullPath);
+                    }
+                    else if (extension == ".exe")
+                    {
+                        Process.Start(fullPath);
+                    }
+                    // 其他类型文件暂不处理
+                }
+            }
+
+        }
+    }
+}
